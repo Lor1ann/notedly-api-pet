a4ecc405e9e63476771642256743df6ccc5bfb73
Eugene <hurakane122@gmail.com>
2022-06-24 17:05:25 +0300

    final

--- src/graphql/resolvers/user.js
+++ src/graphql/resolvers/user.js
@@ -5,6 +5,6 @@ module.exports = {
     return await models.Note.find({ author: user._id }).sort({ _id: -1 });
   },
   favorites: async (user, args, { models }) => {
-    return await models.Note.find({ favoritedBy: user._id }).sort({ _id: -1 });
+    return await models.Note.find({ favoriteBy: user._id }).sort({ _id: -1 });
   },
 };
